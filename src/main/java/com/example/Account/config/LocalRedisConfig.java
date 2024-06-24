48455d7b1b485fa11469ba4f61d5e1b36a934679
Jimin Lee <rjsb0648@gmail.com>
2024-06-24 20:17:38 +0900

    first commit

---
+++ src/main/java/com/example/Account/config/LocalRedisConfig.java
@@ -0,0 +1,37 @@
+package com.example.Account.config;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import redis.embedded.RedisServer;
+
+import java.io.IOException;
+
+@Slf4j
+@Configuration
+public class LocalRedisConfig {
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    private RedisServer redisServer;
+
+    @PostConstruct
+    public void startRedis() throws IOException {
+        redisServer = new RedisServer(redisPort);
+
+        try {
+            redisServer.start();
+        } catch (Exception e) {
+            log.error("Failed to start redis server", e);
+        }
+    }
+
+    @PreDestroy
+    public void stopRedis() throws IOException {
+        if (redisServer != null) {
+            redisServer.stop();
+        }
+    }
+}
---
+++ src/main/java/com/example/Account/domain/Account.java
@@ -0,0 +1,58 @@
+package com.example.Account.domain;
+
+import com.example.Account.exception.AccountException;
+import com.example.Account.type.AccountStatus;
+import com.example.Account.type.ErrorCode;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class Account {
+    @Id
+    @GeneratedValue
+    private Long id;    // pk 지정
+
+    @ManyToOne
+    private AccountUser accountUser;
+    private String accountNumber;
+
+    @Enumerated(EnumType.STRING) // enum 값의 실제 문자열 저장
+    private AccountStatus accountStatus;
+    private Long balance;
+
+    private LocalDateTime registeredAt;
+    private LocalDateTime unRegisteredAt;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    public void useBalance(Long amount) {
+        if (amount > balance) {
+            throw new AccountException(ErrorCode.ACCOUNT_EXCEED_BALANCE);
+        }
+
+        balance -= amount;
+    }
+
+    public void cancelBalance(Long amount) {
+        if (amount < 0) {
+            throw new AccountException(ErrorCode.INVALID_REQUEST);
+        }
+
+        balance += amount;
+    }
+
+}
