477039b7600229b00f17cc7c5e891c80f6c8f2bb
Jimin Lee <rjsb0648@gmail.com>
2024-06-27 00:22:06 +0900

    feedback 수정

--- src/main/java/com/example/Account/type/AccountStatus.java
+++
@@ -1,6 +0,0 @@
-package com.example.Account.type;
-
-public enum AccountStatus {
-    IN_USE,
-    UNREGISTERED
-}
---
+++ src/main/java/com/example/account/domain/Transaction.java
@@ -0,0 +1,32 @@
+package com.example.account.domain;
+
+import com.example.account.type.TransactionResultType;
+import com.example.account.type.TransactionType;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class Transaction extends BaseEntity {
+    @Enumerated(EnumType.STRING)
+    private TransactionType transactionType;
+
+    @Enumerated(EnumType.STRING)
+    private TransactionResultType transactionResultType;
+
+    @ManyToOne
+    private Account account;
+    private Long amount;
+    private Long balanceSnapShot;
+
+    private String transactionId;
+    private LocalDateTime transactedAt;
+}
